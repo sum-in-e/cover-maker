904c20275698740294da2b751adbafd649a37f9a
sumin <suminkim.me@gmail.com>
2023-02-05 17:14:46 +0900

    og image 경로 수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,12 +6,12 @@ class MyDocument extends Document {
       <Html lang="ko">
         <title>COVER MAKER</title>
         <Head>
-          <meta content="width=device-width, initial-scale=1" name="viewport" />
-          <meta name="description" content="내가 만드는 COVER MAKER" />
           <link rel="icon" href="/LOGO_covermaker.svg" />
+          <meta name="description" content="내가 만드는 COVER MAKER" />
+          <meta content="width=device-width, initial-scale=1" name="viewport" />
           <meta property="og:title" content="COVER MAKER" />
           <meta property="og:type" content="website" />
-          <meta property="og:image" content="/public/images/og_image.png" />
+          <meta property="og:image" content="/images/og_image.png" />
           <meta property="og:image:alt" content="내가 만드는 COVER MAKER" />
           <meta property="og:description" content="내가 만드는 COVER MAKER" />
           <meta
