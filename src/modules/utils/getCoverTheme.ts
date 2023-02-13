a0bdd01a06d5e5fedb90bc09bc8328fdea701197
sumin <suminkim.me@gmail.com>
2023-02-13 19:35:07 +0900

    webp 확장자로 테마 교체

--- src/modules/utils/getCoverTheme.ts
+++ src/modules/utils/getCoverTheme.ts
@@ -8,35 +8,35 @@ interface Props {
 export const getCoverTheme = ({ size, theme }: Props) => {
   if (size === "600 x 600") {
     if (theme === "1번 테마") {
-      return "/images/theme/theme1-600x600.png";
+      return "/images/theme/theme1-600x600.webp";
     } else if (theme === "2번 테마") {
-      return "/images/theme/theme2-600x600.png";
+      return "/images/theme/theme2-600x600.webp";
     } else if (theme === "3번 테마") {
-      return "/images/theme/theme3-600x600.png";
+      return "/images/theme/theme3-600x600.webp";
     }
   } else if (size === "960 x 540") {
     if (theme === "1번 테마") {
-      return "/images/theme/theme1-960x540.png";
+      return "/images/theme/theme1-960x540.webp";
     } else if (theme === "2번 테마") {
-      return "/images/theme/theme2-960x540.png";
+      return "/images/theme/theme2-960x540.webp";
     } else if (theme === "3번 테마") {
-      return "/images/theme/theme3-960x540.png";
+      return "/images/theme/theme3-960x540.webp";
     }
   } else if (size === "960 x 450") {
     if (theme === "1번 테마") {
-      return "/images/theme/theme1-960x450.png";
+      return "/images/theme/theme1-960x450.webp";
     } else if (theme === "2번 테마") {
-      return "/images/theme/theme2-960x450.png";
+      return "/images/theme/theme2-960x450.webp";
     } else if (theme === "3번 테마") {
-      return "/images/theme/theme3-960x450.png";
+      return "/images/theme/theme3-960x450.webp";
     }
   } else if (size === "800 x 375") {
     if (theme === "1번 테마") {
-      return "/images/theme/theme1-800x375.png";
+      return "/images/theme/theme1-800x375.webp";
     } else if (theme === "2번 테마") {
-      return "/images/theme/theme2-800x375.png";
+      return "/images/theme/theme2-800x375.webp";
     } else if (theme === "3번 테마") {
-      return "/images/theme/theme3-800x375.png";
+      return "/images/theme/theme3-800x375.webp";
     }
   }
 };
